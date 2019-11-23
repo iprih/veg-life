bb9898bd0d77a31c93572f4ec0bd6b1dffab708c
iprih <priscila.silva076@gmail.com>
2019-11-23 15:53:23 -0300

    estruturação de app e server

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const bodyParser = require('body-parser')
+const app = express()
+
+mongoose.connect("mongodb+srv://admin:admin@cluster0-tjvtu.mongodb.net/restaurantes", {useNewUrlParser: true});
+
+let db = mongoose.connection;
+db.on("error", console.log.bind(console, "connection error:"))
+db.once("open", function(){
+  console.log("conexão feita com sucesso.")
+})
+
+const restaurantes = require("./routes/restauranteRoutes")
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    )
+    next()
+  })
+
+
+app.use(bodyParser.json())
+
+app.use("/restaurantes", restaurantes)
+
+module.exports = app
+
