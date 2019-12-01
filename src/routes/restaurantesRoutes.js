6e7061cad94694f529119d94fc325b90407a44dd
iprih <priscila.silva076@gmail.com>
2019-12-01 17:16:59 -0300

    post restaurante, routes

--- src/routes/restaurantesRoutes.js
+++ src/routes/restaurantesRoutes.js
@@ -1,1 +1,7 @@
-// rotas
+const express = require("express")
+const router = express.Router()
+const controller = require("../controller/restaurantesController")
+
+router.post("/", controller.post)
+
+module.exports = router
