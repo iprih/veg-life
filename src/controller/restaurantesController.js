ab7c4c58a69d49249d0ca5b592d82aa5c007af2a
iprih <priscila.silva076@gmail.com>
2019-12-01 17:14:53 -0300

    post restaurante, controller

--- src/controller/restaurantesController.js
+++ src/controller/restaurantesController.js
@@ -1,1 +1,15 @@
-// controller
+const Restaurantes = require("../model/restaurantes")
+
+exports.post = (req, res) => {
+    const restaurante = new Restaurantes(req.body)
+
+    restaurante.save(function(err){
+        if (err) res.status(500).send(err)
+        else{
+            res.status(201).sebd({
+                status:true,
+                mensagem: "Restaurante cadastrado com sucesso!"
+            })
+        }
+    })
+}
